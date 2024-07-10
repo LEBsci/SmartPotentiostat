a3c2853b5482197a2fa38b016b48868830c430ed
LEBsci <github@botello.me>
2024-07-10 20:38:53 +0200

    barebones of the system

---
+++ PotentiostatAssistant.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# pylint: disable=unused-argument
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple Bot to reply to Telegram messages.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Application and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+# Get the bot token from the environment file
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+bot_token = os.getenv("BOT_TOKEN")
+
+# Get the list of allowed users from the environment file
+allowed_users = []
+users = ["ALBERTO", "ALESSIO", "LLUIS", "LUIS"]
+for user in users:
+    allowed_users.append(os.getenv(user))
+
+# Convert the list of allowed users to a list of integers
+allowed_users = list(map(int, allowed_users))
+
+# Import the required modules
+
+import logging
+
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Help!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(bot_token).build()
+
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler("start", start, filters=filters.User(user_id=allowed_users)))
+    application.add_handler(CommandHandler("help", help_command, filters=filters.User(user_id=allowed_users)))
+
+    # on non command i.e message - echo the message on Telegram
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND & filters.User(user_id=allowed_users), echo))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
